4f74830a4d74d4ebfab307ef7577db8ae69240de
Yaroslav Prokopenko <misterrprokop@gmail.com>
2020-11-19 18:54:25 +0200

    Add two projects with startup settings

---
+++ OrderApi.Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderApi.Domain
+{
+    public partial class Order
+    {
+        public Guid Id { get; set; }
+
+        public int OrderState { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public string CustomerFullName { get; set; }
+    }
+}
