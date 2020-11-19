3cf1379d4c38d71b0be3ed7a968f94a3a2fd2b64
Yaroslav Prokopenko <misterrprokop@gmail.com>
2020-11-19 18:39:18 +0200

    Добавьте файлы проекта.

---
+++ OrderApi/Infrastructure/Automapper/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace OrderApi.Infrastructure.Automapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //TODO
+        }
+    }
+}
---
+++ OrderApi/Validators/OrderModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using OrderApi.Models;
+
+namespace OrderApi.Validators
+{
+    public class OrderModelValidator : AbstractValidator<OrderModel>
+    {
+        public OrderModelValidator()
+        {
+            RuleFor(x => x.CustomerFullName)
+                .NotNull()
+                .WithMessage("The customer name must be not empty");
+
+            RuleFor(x => x.CustomerFullName)
+                .MinimumLength(2)
+                .WithMessage("The customer name must be at least 2 character long");
+        }
+    }
+}
