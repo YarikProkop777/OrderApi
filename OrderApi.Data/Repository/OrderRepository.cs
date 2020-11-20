0d9cea1153c2a0b6f365ab15797608c0404b8dd1
Yaroslav Prokopenko <misterrprokop@gmail.com>
2020-11-20 11:05:24 +0200

    Added repository pattern

---
+++ OrderApi.Data/Repository/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderApi.Data.Database;
+using OrderApi.Domain;
+
+namespace OrderApi.Data.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext orderContext) : base(orderContext)
+        {
+        }
+
+        public async Task<Order> GetOrderByIdAsync(Guid orderId, CancellationToken cancellationToken)
+        {
+            return await orderContext.Order.FirstOrDefaultAsync(order => order.Id == orderId, cancellationToken);
+        }
+
+        public async Task<List<Order>> GetOrdersByCustomerIdAsync(Guid customerId, CancellationToken cancellationToken)
+        {
+            return await orderContext.Order.Where(order => order.CustomerId == customerId).ToListAsync(cancellationToken);
+        }
+
+        public async Task<List<Order>> GetPaidOrdersAsync(CancellationToken cancellationToken)
+        {
+            return await orderContext.Order.Where(order => order.OrderState == 2).ToListAsync(cancellationToken);
+        }
+    }
+}
