aac10e344be44a4b54564de8bcc19cb08d579532
Yaroslav Prokopenko <misterrprokop@gmail.com>
2020-12-27 15:27:41 +0200

    Fixing issue with swagger

--- OrderApi/Startup.cs
+++ OrderApi/Startup.cs
@@ -117,7 +117,7 @@ namespace OrderApi
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/swagger.json", "Order API");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API");
                 c.RoutePrefix = string.Empty;
             });
 
